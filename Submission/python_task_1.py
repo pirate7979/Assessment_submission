313ce478da75c2405c1ce864f7b5b73e74c4e731
pirate7979 <144538200+pirate7979@users.noreply.github.com>
2023-12-09 21:40:55 +0530

    Update python_task_1.py

--- Submission/python_task_1.py
+++ Submission/python_task_1.py
@@ -18,3 +18,45 @@ def get_type_count(df):
 
 df = pd.read_csv(r'C:\/Users\Acer\Desktop\ML\Untitled Folder 1\MapUp-Data-Assessment-F-main\datasets\dataset-1.csv')
 print(get_type_count(df)) 
+
+
+def get_bus_indexes(df):
+  mean= df['bus'].mean()
+  indexes= df[df['bus'] > 2 * mean].index.tolist()
+  indexes.sort()
+  return indexes
+
+df = pd.read_csv(r'C:\/Users\Acer\Desktop\ML\Untitled Folder 1\MapUp-Data-Assessment-F-main\datasets\dataset-1.csv')
+print(get_bus_indexes(df))
+
+
+def filter_routes(df):
+  avg= df.groupby('truck')['route'].mean()
+  routes= avg[avg > 7].tolist()
+  routes.sort()
+  return(filter_routes(df))
+
+df = pd.read_csv(r'C:\/Users\Acer\Desktop\ML\Untitled Folder 1\MapUp-Data-Assessment-F-main\datasets\dataset-1.csv')
+print(filter_routes(df))
+
+
+def multiply_matrix(matrix):
+  new_df= matrix.copy()
+  new_df= new_df.applymap(lambda x: x * 0.75 if x > 20 else x * 1.25)
+  new_df= new_df.round(1)
+  return new_df
+
+df = pd.read_csv(r'C:\/Users\Acer\Desktop\ML\Untitled Folder 1\MapUp-Data-Assessment-F-main\datasets\dataset-1.csv')
+print(multiply_matrix(matrix))
+
+
+def time_check(df):
+  
+  
+
+
+
+
+
+
+
